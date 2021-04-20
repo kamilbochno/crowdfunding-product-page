227fdcfd6c183256e8bc199a0cb54b73aae8469e
Kamil Bochno <kamilas12345@gmail.com>
2021-04-20 13:01:26 +0200

    Scripted bookmark button

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function bookmarked() {
+    document.getElementById("b_button").style.display = "none";
+    document.getElementById("bk_button").style.display = "block";
+}
+
+function bookmark() {
+    document.getElementById("b_button").style.display = "block";
+    document.getElementById("bk_button").style.display = "none";
+}
